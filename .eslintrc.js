1823539448a1a3f311bb8e6556484fda50528646
uncleseneca <uncleseneca@gmail.com>
2018-07-15 12:09:09 +0300

    added solid-soda eslint config and fixed errors

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,3 @@
 module.exports = {
-  extends: ['eslint:recommended'],
-  env: {
-    node: true,
-    commonjs: true,
-    es6: true,
-  },
-  rules: {
-    indent: ['error', 2],
-    'linebreak-style': ['error', 'unix'],
-    quotes: ['error', 'single'],
-    semi: ['error', 'never'],
-    'no-console': 1,
-    'comma-dangle': ['error', 'always-multiline'],
-    'object-curly-spacing': ['error', 'always'],
-    'eol-last': ['error', 'always'],
-  },
+  extends: ['@solid-soda'],
 }
