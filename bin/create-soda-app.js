63d8e1293283bf08276d4787df59b810bf080bde
uncleseneca <uncleseneca@gmail.com>
2018-07-15 13:12:42 +0300

    updated eslint config version and fixed lint issues

--- bin/create-soda-app.js
+++ bin/create-soda-app.js
@@ -5,7 +5,7 @@ const program = require('commander')
 const lib = require('..')
 const pkg = require('../package.json')
 
-const createSodaApp = lib.createSodaApp
+const { createSodaApp } = lib
 
 let projectName
 
@@ -13,15 +13,15 @@ program
   .version(pkg.version)
   .arguments('<project-directory>')
   .usage(`${chalk.green('<project-directory>')} [options]`)
-  .action(function (name) {
+  .action((name) => {
     projectName = name
   })
   .allowUnknownOption()
   .parse(process.argv)
 
-const example = program.example
+const { example } = program
 
 createSodaApp({
   projectName,
-  example
+  example,
 })
