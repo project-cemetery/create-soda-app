9bd184a9e9c502c3ae0ece8b4e905494703b9a95
Shek Evgeniy <shek.e.g@yandex.ru>
2018-07-29 17:39:57 +0600

    added template system

--- bin/create-soda-app.js
+++ bin/create-soda-app.js
@@ -7,13 +7,16 @@ const pkg = require('../package.json')
 
 const { createSodaApp } = lib
 
+let templateName
 let projectName
 
 program
   .version(pkg.version)
   .arguments('<project-directory>')
+  .option('--template <template-directory>')
   .usage(`${chalk.green('<project-directory>')} [options]`)
   .action((name) => {
+    templateName = program.template || 'default'
     projectName = name
   })
   .allowUnknownOption()
@@ -22,6 +25,7 @@ program
 const { example } = program
 
 createSodaApp({
+  templateName,
   projectName,
   example,
 })
